bcc81b501c2592ef15b3ca3291f8fc37b893990d
ValerioNunes <valeriobsno@gmail.com>
2020-02-01 17:27:54 -0300

    Adição dos outros arquivos

---
+++ src/main/java/bancodedados/cidade/Cidade.java
@@ -0,0 +1,82 @@
+package bancodedados.cidade;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Objects;
+
+@Entity
+public class Cidade {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @PositiveOrZero(message="O taxa deve ser preenchido")
+    private double taxa;
+
+    @NotEmpty(message="O UF deve ser preenchido")
+    private String UF;
+
+    @NotEmpty(message="O Nome deve ser preenchido")
+    private String nome;
+
+    public Cidade() {}
+
+    public Cidade(String nome, double taxa, String UF) {
+        this.nome = nome;
+        this.taxa = taxa;
+        this.UF = UF;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double gettaxa() {
+        return taxa;
+    }
+
+    public void settaxa(double taxa) {
+        this.taxa = taxa;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cidade)) return false;
+        Cidade cidade = (Cidade) o;
+        return Double.compare(cidade.taxa, taxa) == 0 &&
+                getId().equals(cidade.getId()) &&
+                getUF().equals(cidade.getUF()) &&
+                getNome().equals(cidade.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), taxa, getUF(), getNome());
+    }
+}
---
+++ src/main/java/bancodedados/cidade/CidadeRepository.java
@@ -0,0 +1,18 @@
+package bancodedados.cidade;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+
+    Cidade findFirstByNome(String nome);
+
+    @Query("SELECT u FROM Cidade u WHERE u.nome = ?1")
+    Cidade buscaPor(String nome);
+
+    @Query(value = "From Cidade")
+    List<Cidade> todos(Sort sort);
+}
